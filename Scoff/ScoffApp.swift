ed728dc9b313eed48665bcc995d6a5cc93bb9579
Scott Brown <scott_m_brown@mac.com>
2020-11-11 14:03:13 +0000

    Initial commit

---
+++ Scoff/ScoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoffApp.swift
+//  Scoff
+//
+//  Created by Scott Brown on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
