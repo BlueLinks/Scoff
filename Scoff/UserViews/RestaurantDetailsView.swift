090797abb472d186025ef40c5f4df9ae473cf550
Scott Brown <scott_m_brown@mac.com>
2021-01-27 17:33:30 +0000

    user can now modify restaurant details

--- Scoff/UserViews/RestaurantDetailsView.swift
+++ Scoff/UserViews/RestaurantDetailsView.swift
@@ -12,7 +12,7 @@ import URLImage
 struct RestaurantDetailsView: View {
     
     let db = Firestore.firestore()
-    @State var restaurant = restaurantRaw(id: "", name: "", picture: "")
+    @State var restaurant = restaurantRaw(id: "", name: "", picture: "", email: "")
     @EnvironmentObject var session: SessionStore
     @State private var imageToDisplay: Image?
     @State private var imageSelected = false
@@ -21,62 +21,110 @@ struct RestaurantDetailsView: View {
     @State private var inputImage: UIImage?
     @State private var uploadProgress : Double = 0.00
     @State private var showUploadProgress = false
+    @State var name : String = ""
+    @State var email : String = ""
+    @State var detailsChanged = false
+    @State var showSaveWarn = false
     
     var body: some View {
         VStack{
             Form{
                 if  let user = session.session{
                     Section(header: Text("Restaurant details")){
-                        Text("\(restaurant.name)")
+                        HStack{
+                            Text("Name:")
+                            Spacer()
+                            TextField("",text: $name).onChange(of: name, perform: { (value) in
+                                if !(value == restaurant.name){
+                                    print("name changed to \(value)")
+                                    detailsChanged = true
+                                } else {
+                                    detailsChanged = false
+                                }
+                            })
+                        }
+                        HStack{
+                            Text("Email:")
+                            Spacer()
+                            TextField("",text: $email).keyboardType(/*@START_MENU_TOKEN@*/.emailAddress/*@END_MENU_TOKEN@*/).onChange(of: email, perform: { (value) in
+                                if !(value == restaurant.email){
+                                    print("Email changed to \(value)")
+                                    detailsChanged = true
+                                } else {
+                                    detailsChanged = false
+                                }
+                            })
+                        }
                         Button(action: {
                             self.showingImagePicker = true
                         }){
-                            VStack{
+                            HStack{
                                 Text("Select image")
+                                Spacer()
                                 if !imageSelected{
                                     if restaurant.picture != ""{
-                                    URLImage(url: URL(string: restaurant.picture)!){ image in
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                    }
+                                        URLImage(url: URL(string: restaurant.picture)!){ image in
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                        }
                                     }
                                 } else {
-                                if imageToDisplay != nil {
-                                    imageToDisplay?
-                                        .resizable()
-                                        .scaledToFit()
+                                    if imageToDisplay != nil {
+                                        imageToDisplay?
+                                            .resizable()
+                                            .scaledToFit()
+                                        
+                                    }
                                 }
-                                }
+                            }.frame(height: 180)
+                            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                                ImagePicker(image: self.$inputImage)
                             }
                         }
-                        HStack{
-                            Button(action: {
-                                uploadImage()
-                            }){
-                                Text("Upload!")
-                            }
-                            Spacer()
-                            if self.showUploadProgress {
-                                Text("\(uploadProgress)")
-                            }
-                        }
-                        
                     }
-                    Section(header: Text("User details")){
-                        Text("\(user.firstName!)")
-                        Text("\(user.lastName!)")
-                        Text("\(user.email!)")
-                        Text("\(user.dateOfBirth!)")
-                    }
+                    
                 }
-                
-            }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-                ImagePicker(image: self.$inputImage)
             }
+            .alert(isPresented:$showSaveWarn){
+                Alert(title: Text("Save?"), message: Text("Are you sure you want to Save?"), primaryButton: .destructive(Text("Save")){
+                    if imageSelected{
+                        uploadImage()
+                    }
+                    saveChanges()
+                    
+                }, secondaryButton: .cancel())
+            }
+            // Button for saving changes
+            .navigationBarItems(trailing: Button(action: {
+                showSaveWarn = true
+            }) {
+                Text("Save").bold()
+            }.disabled(!detailsChanged))
+            .navigationTitle("Restaurant Details")
         }.onAppear(perform: getData)
     }
     
+    
+    func saveChanges(){
+        print("Attempting to save changes to user details")
+        if session.session != nil {
+            let userRef = db.collection("restaurants").document(restaurant.id)
+            userRef.updateData([
+                "name" : name,
+                "email" : email
+            ]){ err in
+                if let err = err {
+                    print("Error updating document: \(err)")
+                } else {
+                    print("Document successfully updated")
+                    detailsChanged = false
+                    restaurant.name = name
+                    restaurant.email = email
+                }
+            }
+        }
+    }
     
     func getData() {
         if let user = session.session{
@@ -88,7 +136,12 @@ struct RestaurantDetailsView: View {
                 }
                 
                 if let restaurant = document{
-                    self.restaurant =  restaurantRaw(id: restaurant.documentID, name: restaurant.get("name") as! String, picture: restaurant.get("splash_image") as! String)
+                    let name = restaurant.get("name") as! String
+                    let email = restaurant.get("email") as! String
+                    self.restaurant =  restaurantRaw(id: restaurant.documentID, name: name, picture: restaurant.get("splash_image") as! String, email: email)
+                    self.name = name
+                    self.email = email
+                    self.detailsChanged = false
                 }
                 
             }
@@ -101,6 +154,7 @@ struct RestaurantDetailsView: View {
         imageToDisplay = Image(uiImage: inputImage)
         image = inputImage
         imageSelected = true
+        detailsChanged = true
     }
     
     func uploadImage(){
@@ -120,6 +174,7 @@ struct RestaurantDetailsView: View {
             }
             // Metadata contains file metadata such as size, content-type.
             let size = metadata.size
+            print("File size: \(size)")
             // You can also access to download URL after upload.
             splashRef.downloadURL { (url, error) in
                 guard let downloadURL = url else {
@@ -127,56 +182,58 @@ struct RestaurantDetailsView: View {
                     // Uh-oh, an error occurred!
                     return
                 }
+                print("downloadURL: \(downloadURL)")
             }
             
         }
-            
+        
         uploadTask.observe(.resume) { snapshot in
             showUploadProgress = true
         }
         
         uploadTask.observe(.progress) { snapshot in
-          // Upload reported progress
+            // Upload reported progress
             showUploadProgress = true
             self.uploadProgress = 100.0 * Double(snapshot.progress!.completedUnitCount)
-            / Double(snapshot.progress!.totalUnitCount)
+                / Double(snapshot.progress!.totalUnitCount)
             print(self.uploadProgress)
         }
         
         uploadTask.observe(.success) { snapshot in
-          showUploadProgress = false
+            showUploadProgress = false
             
             if let user = session.session{
                 splashRef.downloadURL{( url, error) in
-                guard let downloadURL = url else {
-                    print("ERROR")
-                    return
-                }
-                let splashImageUrlString = downloadURL.absoluteString
-                db.collection("restaurants").document(user.restaurantID!).updateData([
-                    "splash_image" : splashImageUrlString
-                ]){ err in
-                    if let err = err {
-                        print("Error changing splash image url of firestore document: \(err)")
-                    } else {
-                        print("URL successfully updated")
+                    guard let downloadURL = url else {
+                        print("ERROR")
+                        return
                     }
-                }
-                
+                    let splashImageUrlString = downloadURL.absoluteString
+                    db.collection("restaurants").document(user.restaurantID!).updateData([
+                        "splash_image" : splashImageUrlString
+                    ]){ err in
+                        if let err = err {
+                            print("Error changing splash image url of firestore document: \(err)")
+                        } else {
+                            print("URL successfully updated")
+                            self.imageSelected = false
+                        }
+                    }
+                    
                 }
                 
             }
         }
-            
-            
-
-                
-                
-                
-            
         
         
-
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     struct RestaurantDetailsView_Previews: PreviewProvider {
