42f443436bb0b4f0ff69ed0c5e7ed27a2ba53e66
Scott Brown <scott_m_brown@mac.com>
2021-04-02 16:07:28 +0100

    Created sheet to fill in payment details

--- Scoff/OrderView.swift
+++ Scoff/OrderView.swift
@@ -10,6 +10,154 @@ import Stripe
 import URLImage
 import Firebase
 
+struct paymentSheet : View {
+    
+    @EnvironmentObject var order : Order
+    @EnvironmentObject var session: SessionStore
+    
+    @Binding var itemsInCart : Bool
+    @State var orderWarn : Bool = false
+    @Binding var showing : Bool
+    var payableAmount : Double = 0.00
+    var tableNumber : Int
+    @State var cardHolderName = ""
+    @State var cardNumber = ""
+    @State var ExpiryMonth = ""
+    @State var ExpiryYear = ""
+    @State var secCode = ""
+    
+    var body: some View {
+        // Button for returing without ordering
+        HStack{
+            Button( action: {
+                showing = false
+            }) {
+                HStack{
+                    Image(systemName : "chevron.left")
+                    Text("Back ")
+                }
+            }.padding()
+            Spacer()
+        }
+        
+        if let restaurant = self.order.restaurant{
+            Text("\(restaurant.name)").font(.title)
+        }
+        
+        Text("£\(self.order.total, specifier: "%.2f")")
+        Form{
+            Section{
+                HStack{
+                    Text("Card Holder Name:")
+                    TextField("John Smith", text: $cardHolderName)
+                }
+                
+                HStack{
+                    Text("Card Number: ")
+                    TextField("**** **** **** ****", text: $cardNumber).keyboardType(.decimalPad)
+                }
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("Expiry Month")
+                        TextField("MM", text: $ExpiryMonth)
+                    }
+                    VStack(alignment: .leading){
+                        Text("Expiry Year")
+                        TextField("YY", text: $ExpiryYear).keyboardType(.decimalPad)
+                    }
+                }
+                HStack{
+                    Text("CVV:")
+                    TextField("***", text: $secCode).keyboardType(.decimalPad)
+                }
+            }
+            Section{
+                Button(action:{
+                    order.orderTime = Date()
+                    placeOrder()
+                    
+                }){
+                    Text("Pay £\(self.order.total, specifier: "%.2f") now")
+                }.buttonStyle(formButtonStyle())
+                .listRowBackground(Color(.systemGroupedBackground))
+            }
+        }.alert(isPresented:$orderWarn){
+            Alert(title: Text("Order Status"), message: Text("Order placed!"), dismissButton: .default(Text("OK"))
+                    {
+                        print("User is ok")
+                        self.order.items = []
+                        self.itemsInCart = false
+                        self.showing = false
+                    })
+        }
+        
+    }
+    
+    func placeOrder(){
+        let db = Firestore.firestore()
+        uploadOrder(dbRef: db.collection("restaurants").document(self.order.restaurant!.id).collection("orders"))
+        if session.session != nil {
+            print("User is logged in")
+            uploadOrder(dbRef: db.collection("users").document(session.session!.uid).collection("orders"))
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+            self.orderWarn.toggle()
+
+        }
+    }
+    
+    func uploadOrder(dbRef: CollectionReference){
+        
+        var orderRef: DocumentReference? = nil
+        
+        orderRef = dbRef.addDocument(data: [
+            "restaurantName" : self.order.restaurant!.name as String,
+            "dateTime" : self.order.orderTime! as Date,
+            "tableNumber" : tableNumber as Int,
+        ]) {
+            err in
+            if let err = err {
+                print("Error adding order: \(err)")
+            } else {
+                print("Order added with ID: \(orderRef!.documentID)")
+                for item in self.order.items{
+                    var itemRef : DocumentReference? = nil
+                    itemRef = dbRef.document(orderRef!.documentID).collection("items").addDocument(data :[
+                        "name" : item.item.name,
+                        "quantity" : item.quantity,
+                        "price" : item.item.price,
+                        "notes" : item.notes,
+                    ]) { err in
+                        if let err = err {
+                            print("Error adding item: \(err)")
+                        } else {
+                            print("Item added with ID: \(itemRef!.documentID)")
+                            for extra in item.extras{
+                                var extraRef : DocumentReference? = nil
+                                extraRef = dbRef.document(orderRef!.documentID).collection("items").document(itemRef!.documentID).collection("extras").addDocument(data :[
+                                    "name" : extra.name,
+                                    "price" : extra.price
+                                ]) {
+                                    err in
+                                    if let err = err {
+                                        print("Error adding extra: \(err)")
+                                    } else {
+                                        print("Extra added with ID: \(extraRef!.documentID)")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        print("Order uploaded")
+        
+    }
+    
+}
+
 struct OrderView: View {
     
     
@@ -24,21 +172,21 @@ struct OrderView: View {
         return tableNumberPicker + 1
     }
     
+    @State var showingPaymentSheet : Bool = false
     @State private var tableWarn : Bool = false
-    @State var orderWarn : Bool = false
     
     var body: some View {
         NavigationView{
             VStack{
-                if itemsInCart {
+                if self.order.items.count > 0 {
                     // if items are in the cart then show the checkout
-                    List{
+                    Form{
                         if let restaurant = self.order.restaurant{
                             Section{
                                 HStack(){
                                     Text(restaurant.name)
                                         .font(.title)
-                                Spacer()
+                                    Spacer()
                                     URLImage(url: URL(string: restaurant.picture)!){ image in
                                         image
                                             .resizable()
@@ -96,8 +244,7 @@ struct OrderView: View {
                             }.alert(isPresented:$tableWarn){
                                 Alert(title: Text("Table Number"), message: Text("Are you sure you're at table \(tableNumber)?"), primaryButton: .destructive(Text("I'm Sure")){
                                     print("They are sure")
-                                    order.orderTime = Date()
-                                    placeOrder()
+                                    showingPaymentSheet = true
                                     
                                 }, secondaryButton: .cancel())
                             }
@@ -106,29 +253,29 @@ struct OrderView: View {
                                 Text("Total")
                                 Spacer()
                                 Text("£\(self.order.total, specifier: "%.2f")")
+                            }.sheet(isPresented: $showingPaymentSheet){
+                                paymentSheet(itemsInCart: $itemsInCart, showing: $showingPaymentSheet, tableNumber: tableNumber)
                             }
                             
+                        }
+                        Section{
                             Button(action : {
                                 //Checkout button
                                 print("Checkout button pressed")
                                 tableWarn.toggle()
                             }){
                                 HStack{
-                                    Spacer()
-                                    Text("Checkout").font(.title)
-                                        .padding()
-                                        .background(Color.blue)
-                                        .clipShape(Capsule())
-                                        .foregroundColor(.white)
-                                    Spacer()
+                                    
+                                    Text("Checkout")
+                                    
                                 }
-                            }
-                            .alert(isPresented:$orderWarn){
-                                Alert(title: Text("Order Status"), message: Text("Order placed!"), dismissButton: .default(Text("OK")))
-                            }
-                            
+                            }.buttonStyle(formButtonStyle())
+                            .listRowBackground(Color(.systemGroupedBackground))
                         }
-                    }.listStyle(GroupedListStyle())
+                            
+                            
+                        
+                    }
                 } else {
                     // if no items in the order then suggest the user add items
                     Text("Add some items to your cart!")
@@ -163,64 +310,7 @@ struct OrderView: View {
         }
     }
     
-    func placeOrder(){
-        let db = Firestore.firestore()
-        uploadOrder(dbRef: db.collection("restaurants").document(self.order.restaurant!.id).collection("orders"))
-        if session.session != nil {
-            print("User is logged in")
-            uploadOrder(dbRef: db.collection("users").document(session.session!.uid).collection("orders"))
-        }
-        self.orderWarn.toggle()
-    }
     
-    func uploadOrder(dbRef: CollectionReference){
-        
-        var orderRef: DocumentReference? = nil
-        
-        orderRef = dbRef.addDocument(data: [
-            "restaurantName" : order.restaurant!.name as String,
-            "dateTime" : order.orderTime! as Date,
-            "tableNumber" : tableNumber as Int,
-        ]) {
-            err in
-            if let err = err {
-                print("Error adding order: \(err)")
-            } else {
-                print("Order added with ID: \(orderRef!.documentID)")
-                for item in self.order.items{
-                    var itemRef : DocumentReference? = nil
-                    itemRef = dbRef.document(orderRef!.documentID).collection("items").addDocument(data :[
-                        "name" : item.item.name,
-                        "quantity" : item.quantity,
-                        "price" : item.item.price,
-                        "notes" : item.notes,
-//                        "extras" : item.extras.map{$0.name}
-                    ]) { err in
-                        if let err = err {
-                            print("Error adding item: \(err)")
-                        } else {
-                            print("Item added with ID: \(itemRef!.documentID)")
-                            for extra in item.extras{
-                                var extraRef : DocumentReference? = nil
-                                extraRef = dbRef.document(orderRef!.documentID).collection("items").document(itemRef!.documentID).collection("extras").addDocument(data :[
-                                    "name" : extra.name,
-                                    "price" : extra.price
-                                ]) {
-                                    err in
-                                    if let err = err {
-                                        print("Error adding extra: \(err)")
-                                    } else {
-                                        print("Extra added with ID: \(extraRef!.documentID)")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                
-            }
-        }
-    }
     
 }
 
